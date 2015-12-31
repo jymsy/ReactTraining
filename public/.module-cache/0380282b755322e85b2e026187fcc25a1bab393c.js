64e16a20a968114a02df742a37c37f6ae6198077
jymsy <jym20083617@163.com>
2015-12-31 16:45:14 +0800

    init

---
+++ public/.module-cache/0380282b755322e85b2e026187fcc25a1bab393c.js
@@ -0,0 +1,110 @@
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+var Comment = React.createClass({displayName: "Comment",
+  rawMarkup: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return { __html: rawMarkup };
+  },
+
+  render: function() {
+    return (
+      React.createElement("div", {className: "comment"}, 
+        React.createElement("h2", {className: "commentAuthor"}, 
+          this.props.author
+        ), 
+        React.createElement("span", {dangerouslySetInnerHTML: this.rawMarkup()})
+      )
+    );
+  }
+});
+
+var CommentList = React.createClass({displayName: "CommentList",
+  render: function() {
+    var commentNodes = this.props.data.map(function (comment, i) {
+      return (
+        React.createElement(Comment, {author: comment.author, key: comment.id}, 
+          comment.text
+        )
+      );
+    });
+    return (
+      React.createElement("div", {className: "commentList"}, 
+        commentNodes
+      )
+    );
+  }
+});
+
+var CommentForm = React.createClass({displayName: "CommentForm",
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.refs.author.value.trim();
+    var text = this.refs.text.value.trim();
+    if (!text || !author) {
+      return;
+    }
+    this.props.onCommentSubmit({author: author, text: text});
+    this.refs.author.value = '';
+    this.refs.text.value = '';
+    return;
+  },
+  render: function() {
+    return (
+      React.createElement("form", {className: "commentForm", onSubmit: this.handleSubmit}, 
+        React.createElement("input", {type: "text", placeholder: "Your name", ref: "author"}), 
+        React.createElement("input", {type: "text", placeholder: "Say something...", ref: "text"}), 
+        React.createElement("input", {type: "submit", value: "Post"})
+      )
+    );
+  }
+});
+
+
+var CommentBox = React.createClass({displayName: "CommentBox",
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleCommentSubmit: function(comment) {
+    // TODO: submit to the server and refresh the list
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    return (
+      React.createElement("div", {className: "commentBox"}, 
+        React.createElement("h1", null, "Comments"), 
+        React.createElement(CommentList, {data: this.state.data}), 
+        React.createElement(CommentForm, {onCommentSubmit: this.handleCommentSubmit})
+      )
+    );
+  }
+});
+
+ReactDOM.render(
+  React.createElement(CommentBox, {url: "/api/comments", pollInterval: 2000}),
+  document.getElementById('content')
+);
+
+// ReactDOM.render(
+//   <CommentBox data={data} />,
+//   document.getElementById('content')
+// );
+
---
+++ public/.module-cache/df7e4f3254786a24ce6611ff1991561945fde61c.js
@@ -0,0 +1,135 @@
+var data = [
+  {author: "Pete Hunt", text: "This is one comment"},
+  {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
+var Comment = React.createClass({displayName: "Comment",
+  rawMarkup: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return { __html: rawMarkup };
+  },
+
+  render: function() {
+    return (
+      React.createElement("div", {className: "comment"}, 
+        React.createElement("h2", {className: "commentAuthor"}, 
+          this.props.author
+        ), 
+        React.createElement("span", {dangerouslySetInnerHTML: this.rawMarkup()})
+      )
+    );
+  }
+});
+
+var CommentList = React.createClass({displayName: "CommentList",
+  render: function() {
+    var commentNodes = this.props.data.map(function (comment, i) {
+      return (
+        React.createElement(Comment, {author: comment.author, key: comment.id}, 
+          comment.text
+        )
+      );
+    });
+    return (
+      React.createElement("div", {className: "commentList"}, 
+        commentNodes
+      )
+    );
+  }
+});
+
+
+
+var CommentForm = React.createClass({displayName: "CommentForm",
+  render: function() {
+    return (
+      React.createElement("div", {className: "commentForm"}, 
+        "Hello, world! I am a CommentForm."
+      )
+    );
+  }
+});
+
+var CommentForm = React.createClass({displayName: "CommentForm",
+  render: function() {
+    return (
+      React.createElement("form", {className: "commentForm"}, 
+        React.createElement("input", {type: "text", placeholder: "Your name"}), 
+        React.createElement("input", {type: "text", placeholder: "Say something..."}), 
+        React.createElement("input", {type: "submit", value: "Post"})
+      )
+    );
+  }
+});
+
+var CommentBox = React.createClass({displayName: "CommentBox",
+  getInitialState: function() {
+    return {data: []};
+  },
+  //componentDidMount 是一个组件渲染的时候被 React 自动调用的方法。
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  render: function() {
+    return (
+      React.createElement("div", {className: "commentBox"}, 
+        React.createElement("h1", null, "Comments"), 
+        React.createElement(CommentList, {data: this.state.data}), 
+        React.createElement(CommentForm, null)
+      )
+    );
+  }
+});
+
+var CommentBox = React.createClass({displayName: "CommentBox",
+  loadCommentsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    return (
+      React.createElement("div", {className: "commentBox"}, 
+        React.createElement("h1", null, "Comments"), 
+        React.createElement(CommentList, {data: this.state.data}), 
+        React.createElement(CommentForm, null)
+      )
+    );
+  }
+});
+
+ReactDOM.render(
+  React.createElement(CommentBox, {url: "/api/comments", pollInterval: 2000}),
+  document.getElementById('content')
+);
+
+// ReactDOM.render(
+//   <CommentBox data={data} />,
+//   document.getElementById('content')
+// );
+
